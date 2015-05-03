1b5ef0b9344901bba350b94b1333d964b878c1b7
Gillinghammer <colingillingham@gmail.com>
2015-05-03 11:07:14 -0700

    simple todo app

---
+++ public/javascripts/app.js
@@ -0,0 +1,19 @@
+
+// root module for angular app
+var TodoApp = angular.module("TodoApp", []);
+// first angular controller w scope
+TodoApp.controller("TodosCtrl", ["$scope", function ($scope) {
+  $scope.todos    = [];
+  $scope.newTodo  = {};
+
+  $scope.addTodo  = function () {
+    $scope.todos.push($scope.newTodo);
+    $scope.newTodo = {};
+  };
+
+  $scope.delete = function () {
+    console.log(this)
+    $scope.todos.splice(this.$index, 1);
+  }
+
+}]);
